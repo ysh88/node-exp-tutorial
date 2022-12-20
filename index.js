c6c884af7ea2670e63d85d853619a73ad74f1d62
ysh88 <ysh88porcorosso@gmail.com>
2022-12-20 23:14:58 +0900

    creating a web server only /w Node

--- index.js
+++
@@ -1,5 +0,0 @@
-const { format } = require('date-fns');
-const { v4: uuid } = require('uuid'); //import v4 package with an alias as uuid
-
-console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
-console.log(uuid());
